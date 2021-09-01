712c5c8ddc1867f03c40b406411b9fe20d555334
Caius Durling <dev@caius.name>
2021-09-01 20:42:19 +0100

    Test all three built binaries

---
+++ .semaphore/cmd/test-sensu-go-backend.sh
@@ -0,0 +1,9 @@
+cd /
+artifact pull workflow sensu-backend
+chmod +x sensu-backend
+# Belt & braces checks
+./sensu-backend version | grep sensu-backend
+./sensu-backend version | grep "built $(date +%Y-%m-%d)"
+./sensu-backend version | grep "version ${SENSU_GO_VERSION}+ce, community edition"
+# Includes a SHA
+./sensu-backend version | grep ', build '
---
+++ .semaphore/cmd/test-sensu-go-ctl.sh
@@ -0,0 +1,9 @@
+cd /
+artifact pull workflow sensuctl
+chmod +x sensuctl
+# Belt & braces checks
+./sensuctl version | grep sensuctl
+./sensuctl version | grep "built $(date +%Y-%m-%d)"
+./sensuctl version | grep "version ${SENSU_GO_VERSION}+ce, community edition"
+# Includes a SHA
+./sensuctl version | grep ', build '
