a934434e217213446788baa4d3bc0652a6b8e629
Caius Durling <dev@caius.name>
2021-09-01 21:47:03 +0100

    Rebuild the repackage commands for agent

--- .semaphore/cmd/repackage-sensu-go-agent-deb.sh
+++ .semaphore/cmd/repackage-sensu-go-agent-deb.sh
@@ -1,9 +1,9 @@
-cd /
-mkdir sensu-go-agent
+pushd /mnt
 
 # Grab & unpack the deb
 curl -sfL -o ${SENSU_GO_PKG_NAME} https://packagecloud.io/sensu/stable/packages/debian/stretch/${SENSU_GO_PKG_NAME}/download.deb
-dpkg-deb -x ${SENSU_GO_PKG_NAME} sensu-go-agent
+dpkg-deb --vextract ${SENSU_GO_PKG_NAME} sensu-go-agent
+dpkg-deb --control ${SENSU_GO_PKG_NAME} sensu-go-agent/DEBIAN
 
 pushd sensu-go-agent
 
@@ -12,9 +12,22 @@ rm usr/sbin/sensu-agent
 artifact pull workflow sensu-agent --destination usr/sbin/sensu-agent
 
 # Update md5sum with new binary's hash
-sed -e '/usr\\/sbin\\/sensu-agent/ d' -i '' md5sum
-md5sum usr/sbin/sensu-agent >> md5sum
+sed '/usr\/sbin\/sensu-agent/d' -i'' DEBIAN/md5sums
+md5sum usr/sbin/sensu-agent >> DEBIAN/md5sums
+
+# Update the version (append ~ce)
+sed -re 's/^(Version: .+)$/\1~ce/' \
+  -e 's#^(Homepage:).+$#\1 https://github.com/sensu/sensu-go#' \
+  -e 's/^(Maintainer:).+$/\1 Caius <sensu-go-oss@caius.name>/' \
+  -e 's/^(Installed-Size:).+$/\1 '$(du -sk . | awk '{ print $1 }')'/' \
+  -i'' DEBIAN/control
+
+# dpkg-deb whinges if we don't do this first
+chmod 755 DEBIAN/{postinst,postrm,preinst,prerm}
+
+popd
 
 # Package everything back up, add -ce after version in deb filename
-dpkg-deb --build sensu-go-agent ${SENSU_GO_PKG_NAME/${SENSU_GO_VERSION#v}/${SENSU_GO_VERSION#v}-ce}
-artifact push workflow --expires-in 1w ${SENSU_GO_PKG_NAME/${SENSU_GO_VERSION#v}/${SENSU_GO_VERSION#v}-ce}
+SENSU_CE_PKG_NAME=$(echo $SENSU_GO_PKG_NAME | sed -re 's/('${SENSU_GO_VERSION#v}'-[0-9]+)/\1-ce/')
+dpkg-deb --build sensu-go-agent $SENSU_CE_PKG_NAME
+artifact push workflow --expire-in 1w $SENSU_CE_PKG_NAME
