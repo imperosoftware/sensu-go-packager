6d2872c222140f8e925731ce391f4104455e54a0
Caius Durling <dev@caius.name>
2021-09-02 22:37:25 +0100

    Fix release of sensu-go-agent

--- .semaphore/cmd/deploy-sensu-go-agent.sh
+++ .semaphore/cmd/deploy-sensu-go-agent.sh
@@ -2,4 +2,4 @@ SENSU_GO_PKG_NAME=$(curl -sL https://packagecloud.io/sensu/stable/debian/dists/s
 SENSU_CE_PKG_NAME=$(echo $SENSU_GO_PKG_NAME | sed -re 's/('${SENSU_GO_VERSION}'-[0-9]+)/\1-ce/')
 
 artifact pull workflow $SENSU_CE_PKG_NAME
-curl -F package=@$SESU_CE_PKG_NAME --user "${GEMFURY_TOKEN}" https://push.fury.io/impero/
+curl -F package=@$SENSU_CE_PKG_NAME --user "${GEMFURY_TOKEN}:" https://push.fury.io/impero/
